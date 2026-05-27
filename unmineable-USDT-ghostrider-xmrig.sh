fddef362e2b72e6f14f70c90a4abca9e43ddf6f1
qiqi258 <134107390+qiqi258@users.noreply.github.com>
2026-05-28 01:08:20 +0800

    Update unmineable-USDT-ghostrider-xmrig.sh

--- unmineable-USDT-ghostrider-xmrig.sh
+++ unmineable-USDT-ghostrider-xmrig.sh
@@ -1,1 +1,1 @@
-./xmrig -a randomx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TDFRoYVFwze54ojydkiPUja8Twix9X5QRR.qiqi-1#ek8v-txze -p x 
+./xmrig -a randomx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TDFRoYVFwze54ojydkiPUja8Twix9X5QRR.qiqi-1#ek8v-txze -p x
